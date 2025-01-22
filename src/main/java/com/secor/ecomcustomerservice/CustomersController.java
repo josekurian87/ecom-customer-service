8d3b90a6fa83b42cd684e366d21d78d204b8bdbc
jose-kurian <josekurian87@gmail.com>
2025-01-23 00:42:12 +0530

    Code refactored

--- src/main/java/com/secor/ecomcustomerservice/CustomersController.java
+++ src/main/java/com/secor/ecomcustomerservice/CustomersController.java
@@ -1,6 +1,8 @@
 package com.secor.ecomcustomerservice;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,8 @@ import java.util.List;
 @RequestMapping("/customers")
 public class CustomersController {
 
+    private final Logger LOG = LoggerFactory.getLogger(CustomersController.class);
+
     @Autowired
     private CustomersRepository customersRepository;
 
@@ -18,16 +22,19 @@ public class CustomersController {
 
     @GetMapping
     public List<Customers> getAllCustomers() {
+        LOG.info("getAllCustomers");
         return customersRepository.findAll();
     }
 
     @PostMapping
     public Customers addCustomer(@RequestBody Customers customer) {
+        LOG.info("addCustomer for customer {}", customer);
         return customersRepository.save(customer);
     }
 
     @PutMapping("/{id}")
     public Customers updateCustomer(@PathVariable Long id, @RequestBody Customers customerDetails) throws JsonProcessingException {
+        LOG.info("updateCustomer for customer {}", customerDetails);
         Customers customer = customersRepository.findById(id).orElseThrow();
         customer.setFirstName(customerDetails.getFirstName());
         customer.setLastName(customerDetails.getLastName());
@@ -39,6 +46,7 @@ public class CustomersController {
 
     @DeleteMapping("/{id}")
     public void deleteCustomer(@PathVariable Long id) {
+        LOG.info("deleteCustomer for customer {}", id);
         customersRepository.deleteById(id);
     }
 }
